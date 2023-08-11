8e2de09ada0e67941e7b3ece7b39d1eff602aa3b
BrendenTaylor <86581982+BrendenTaylor@users.noreply.github.com>
2023-08-10 21:00:41 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://{deployed-url}.us-east-1.amazonaws.com’
 };
 
 /*
