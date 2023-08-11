105195f64eaf518479e9cf419add028898afd253
BrendenTaylor <86581982+BrendenTaylor@users.noreply.github.com>
2023-08-10 21:01:36 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://{deployed-url}.us-east-1.amazonaws.com’
+  api_url: 'https://56sf75mml0.execute-api.us-east-1.amazonaws.com/API’
 };
 
 /*
